c426e6d7a9494f5a53e0b1203ae2232d7f00aecc
John Robinson <john.robinson@wellzesta.com>
2016-01-27 10:41:35 -0500

    added firebase support

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'maggie-website',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://magelrob.firebaseIO.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
