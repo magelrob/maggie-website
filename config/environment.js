00f475c90f5a7ecde60da6c7a275e60822dcf4b4
John Robinson <john.robinson@wellzesta.com>
2016-01-27 08:53:55 -0500

    added ember-cli-github-pages

--- config/environment.js
+++ config/environment.js
@@ -2,10 +2,10 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'maggie-website',
+    modulePrefix: 'magelrob.github.io',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/magelrob.github.io',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
