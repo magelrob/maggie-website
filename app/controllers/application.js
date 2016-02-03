b3878b18b0d7e98dd535c1dab7e619f092c929be
John Robinson <john.robinson@wellzesta.com>
2016-02-03 06:10:47 -0500

    added navigation

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	
 	nav: {
-		widgets: 'item',
-		index: 'item',
-		buster: 'item'
+		isWidgetsActive: true,
+		isIndexActive: 'active',
+		isBusterActive: true
 	},
-	
 	actions: {
-		widgetsButtonHandler: function (){
-			console.log('widgets button pressed');
-			this.set('nav.widgets','active item');
-			this.transitionTo('widgets');
+		menuClickedHandler(item) {
+			console.log('button ' + item + ' pressed');
+			$('.ui .item').removeClass('active');
+			var sel = '#' + item;
+			$(sel).addClass('active');
+			this.transitionToRoute(item);
+		},
+		toggleSidebar() {
+			$('.ui.sidebar').sidebar('toggle');
 		}
 	}
 });
--- app/controllers/widgets.js
+++ app/controllers/widgets.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     itemActions: [ "Edit", "Remove", "Hide"],
     selectedAction: null,
-
+    someHTML : '<h1>A title &nbsp; &nbsp; &nbsp; &nbsp; title</h1>',
+    author: {
+    	name: 'Maggie',
+    	website: 'http://wellzesta.com'
+    },
     actions: {
       update_selected: function(component, id, value) {
         this.set('selectedAction', id);
